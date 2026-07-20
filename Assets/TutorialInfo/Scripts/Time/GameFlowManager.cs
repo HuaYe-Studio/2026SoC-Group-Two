15041d1091ad26a0e059b759f57e70a636f220af
yugzizhi <18856922180@163.com>
2026-07-20 16:44:00 +0800

    新增了时间系统和只是退出的退出功能（未测试）

---
+++ Assets/TutorialInfo/Scripts/Time/GameFlowManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System;
+using WorldTime;
+public class GameFlowManager : MonoBehaviour
+{
+    private DateTime gameEndTime;
+    private int gameTotalDay = 7;
+
+    private void Start()
+    {
+        if (TimeManager.instance == null)
+        {
+            this.enabled = false;
+            return;
+        }
+        
+        gameEndTime = TimeManager.instance.gameStartTime.AddDays(gameTotalDay);
+
+        if (TimeManager.instance.currentTime >= gameEndTime)
+            return;
+        
+        TimeManager.instance.OnHourChanged += CheckGameEnd;
+    }
+    private void Update()
+    {
+        //待inputsystem替换，不确定是否为空格先用着
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            QuitGame();
+        }
+    }
+    private void OnDestroy()
+    {
+        if (TimeManager.instance != null)
+        {
+            TimeManager.instance.OnHourChanged -= CheckGameEnd;
+        }
+    }
+
+    private void QuitGame()
+    {
+        Debug.Log("退出游戏");
+        
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+    private void CheckGameEnd(DateTime checkTime)
+    {
+        if (checkTime >= gameEndTime)
+        {
+            TimeManager.instance.OnHourChanged -= CheckGameEnd;
+            OnGameEnd();
+        }
+    }
+
+    private void OnGameEnd()
+    {
+        QuitGame();
+    }
+}
+
---
+++ Assets/TutorialInfo/Scripts/Time/TimeManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System;
+
+namespace WorldTime
+{
+    public class TimeManager : MonoBehaviour
+    {
+        [Header("Time Flow Settings")]
+        [Tooltip("时间流速")][SerializeField] private float timeMultiplier = 60f;
+        [SerializeField] private float timeScale = 1f;
+
+        [Header("Initial Time Settings")] 
+        public DateTime gameStartTime { get; private set; }
+        [SerializeField] private int startYear = 1;
+        [SerializeField] private int startMonth = 1;
+        [SerializeField] private int startDay = 1;
+        [SerializeField] private int startHour = 8;
+        [SerializeField] private int startMinute = 0;
+        private int startSecond = 0;
+        
+        public DateTime currentTime { get; private set; }
+ 
+        private int lastDay;
+        private int lastHour;
+        private int lastMinute;
+
+        public event Action<DateTime> OnDayChanged;
+        public event Action<DateTime> OnHourChanged;
+        public event Action<DateTime> OnMinuteChanged;
+        
+        public static TimeManager instance;
+        private void Awake()
+        {
+            if (instance != null && instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            InitializeTime();
+        }
+
+        private void InitializeTime()
+        {
+            gameStartTime = new DateTime(startYear, startMonth, startDay, startHour, startMinute, startSecond);
+            currentTime = gameStartTime;
+
+            lastDay = currentTime.Day;
+            lastHour = currentTime.Hour;
+            lastMinute = currentTime.Minute;
+        }
+
+        private void Update()
+        {
+            float passedTime = Time.deltaTime * timeMultiplier * timeScale;
+            TimeSpan passedTimeSpan = TimeSpan.FromSeconds(passedTime);
+            currentTime = currentTime.Add(passedTimeSpan);
+
+            CheckTime();
+        }
+
+        private void CheckTime()
+        {
+            if (currentTime.Minute != lastMinute)
+            {
+                lastMinute = currentTime.Minute;
+                OnMinuteChanged?.Invoke(currentTime);
+                if (currentTime.Hour != lastHour)
+                {
+                    lastHour = currentTime.Hour;
+                    OnHourChanged?.Invoke(currentTime);
+                    if (currentTime.Day != lastDay)
+                    {
+                        lastDay = currentTime.Day;
+                        OnDayChanged?.Invoke(currentTime);
+                    }
+                }
+            }
+        }
+        //这里应有留给存读档的接口，但因为未明确存读档方式所以不写了
+        //如果之后开局时间需要动态生成需要save start和current两个时间
+    }
+}
+
