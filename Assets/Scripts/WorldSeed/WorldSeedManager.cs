81b56cb23885ec9500b7fa978c7b3416016d8485
yugzizhi <18856922180@163.com>
2026-08-14 17:37:27 +0800

    feat:增加了世界种子生成功能和新的全局退出

---
+++ Assets/Scripts/WorldSeed/WorldSeedManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace WorldSeed
+{
+    public class WorldSeedManager: MonoBehaviour
+    {
+        public static WorldSeedManager Instance{get; private set;}
+        
+        public string CurrentStringSeed{get; private set;}
+        public int CurrentNumSeed{get; private set;}
+        
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+        }
+
+        public void InitializeWorldSeed(string inputSeed = "")
+        {
+            if (string.IsNullOrEmpty(inputSeed))
+            {
+                CurrentNumSeed = Guid.NewGuid().GetHashCode();
+                CurrentStringSeed = CurrentNumSeed.ToString();
+            }
+            else
+            {
+                CurrentStringSeed = inputSeed;
+                CurrentNumSeed= DeterministicHash.GetStringHash(inputSeed);
+            }
+            Debug.Log($"世界种子生成，字符串为{CurrentStringSeed},数字为{CurrentNumSeed}");
+        }
+
+        public System.Random GetRNGForSystem(string system)
+        {
+            int nameHash = DeterministicHash.GetStringHash(system);
+            int derivedSeed = DeterministicHash.Combine(CurrentNumSeed, nameHash);
+            
+            return new System.Random(derivedSeed);
+        }
+
+        public System.Random GetRNGForChunk(int chunkX, int chunkY)
+        {
+            int derivedSeed = DeterministicHash.Combine(CurrentNumSeed, chunkX, chunkY);
+            
+            return new System.Random(derivedSeed);
+        }
+    }
+}
