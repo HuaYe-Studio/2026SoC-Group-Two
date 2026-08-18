ce8c6155986d6c54bd644a462e08fd6ddbaf459f
Sosyaa-Yanzhi <1077511618@qq.com>
2026-08-19 01:12:41 +0800

    将状态UI脚本中的卫生改为心态值，但保留原有显示方案

--- Assets/Scripts/Status_UI/Hygiene_UI.cs
+++ Assets/Scripts/Status_UI/Hygiene_UI.cs
@@ -4,53 +4,53 @@ using Status;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Hygiene_UI : MonoBehaviour
+public class Mental_UI : MonoBehaviour
 {
     [Header("卫生状态图标")]
-    public Image hygiene_icon_good; // 卫生 - 妙
-    public Image hygiene_icon_worse; // 卫生 - 不妙 
-    public Image hygiene_icon_bad; // 卫生 - 我chovy
+    public Image mental_icon_good; // 卫生 - 妙
+    public Image mental_icon_worse; // 卫生 - 不妙 
+    public Image mental_icon_bad; // 卫生 - 我chovy
 
-    private float currentHygieneValue;
-    private float maxHygieneValue;
-    private float minHygieneValue;
+    private float currentMentalValue;
+    private float maxMentalValue;
+    private float minMentalValue;
 
     void Start()
     {
         if (GameFlowManager.Instance == null || !GameFlowManager.Instance.isPlaying)
             return;
-        maxHygieneValue = StatusManager.Instance.GetStatusModule(StatusType.Hygiene).MaxValue;
-        minHygieneValue = StatusManager.Instance.GetStatusModule(StatusType.Hygiene).MinValue;
+        maxMentalValue = StatusManager.Instance.GetStatusModule(StatusType.Mental).MaxValue;
+        minMentalValue = StatusManager.Instance.GetStatusModule(StatusType.Mental).MinValue;
     }
 
     void Update()
     {
         if (GameFlowManager.Instance == null || !GameFlowManager.Instance.isPlaying)
             return;
-        currentHygieneValue = StatusManager.Instance.GetStatusModule(StatusType.Hygiene).CurrentValue;
-        ChangeIconStyle(currentHygieneValue);
+        currentMentalValue = StatusManager.Instance.GetStatusModule(StatusType.Mental).CurrentValue;
+        ChangeIconStyle(currentMentalValue);
     }
 
     #region 改变卫生图标样式
-    void ChangeIconStyle(float currentHygieneValue)
+    void ChangeIconStyle(float currentMentalValue)
     {
-        if (currentHygieneValue > 2 / 3 * maxHygieneValue)
+        if (currentMentalValue > 2 / 3 * maxMentalValue)
         {
-            hygiene_icon_good.gameObject.SetActive(true);
-            hygiene_icon_bad.gameObject.SetActive(false);
-            hygiene_icon_worse.gameObject.SetActive(false);
+            mental_icon_good.gameObject.SetActive(true);
+            mental_icon_bad.gameObject.SetActive(false);
+            mental_icon_worse.gameObject.SetActive(false);
         }
-        else if (currentHygieneValue >= 1 / 3 * maxHygieneValue && currentHygieneValue <= 2 / 3 * maxHygieneValue)
+        else if (currentMentalValue >= 1 / 3 * maxMentalValue && currentMentalValue <= 2 / 3 * maxMentalValue)
         {
-            hygiene_icon_good.gameObject.SetActive(false);
-            hygiene_icon_bad.gameObject.SetActive(false);
-            hygiene_icon_worse.gameObject.SetActive(true);
+            mental_icon_good.gameObject.SetActive(false);
+            mental_icon_bad.gameObject.SetActive(false);
+            mental_icon_worse.gameObject.SetActive(true);
         }
-        else if (currentHygieneValue < 1 / 3 * maxHygieneValue)
+        else if (currentMentalValue < 1 / 3 * maxMentalValue)
         {
-            hygiene_icon_good.gameObject.SetActive(false);
-            hygiene_icon_bad.gameObject.SetActive(true);
-            hygiene_icon_worse.gameObject.SetActive(false);
+            mental_icon_good.gameObject.SetActive(false);
+            mental_icon_bad.gameObject.SetActive(true);
+            mental_icon_worse.gameObject.SetActive(false);
         }
     }
     #endregion
