0917353015d3cf262232493c89e45035b79729f9
yugzizhi <18856922180@163.com>
2026-08-19 15:00:43 +0800

    feat:新增了天气日程表，现在可以根据设定的日程自动切换天空盒了

--- Assets/Scripts/Environment/EnvironmentManager.cs
+++ Assets/Scripts/Environment/EnvironmentManager.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using WorldTime;
+using System.Threading.Tasks;
 
 namespace Environment
 {
@@ -7,10 +10,20 @@ namespace Environment
     {
         public static EnvironmentManager Instance{ get; private set; }
         
+        [Header("场景光源")]
         [SerializeField]private Light sunLight;
+        
+        [Header("SO配置")]
         [SerializeField]private List<SkySettingsSO>  skySettings = new List<SkySettingsSO>();
-        private Dictionary<WeatherType, SkySettingsSO> skySettingsMap = new Dictionary<WeatherType, SkySettingsSO>();
+        private Dictionary<(TimePeriod,WeatherType), SkySettingsSO> skySettingsMap = new Dictionary<(TimePeriod,WeatherType), SkySettingsSO>();
         private SkySettingsSO currentSetting;
+        private TimePeriod currentTimePeriod = TimePeriod.Day;
+        private WeatherType currentWeatherType = WeatherType.Sunny;
+        
+        [Header("时间切换配置")]
+        [SerializeField]private List<SkySchedule> dailySchedules = new List<SkySchedule>();
+        [SerializeField]private float transitionDuration = 10f;
+        private bool isTransitioning = false;
 
         public float CurrentSunIntensityMultiplier =>
             currentSetting != null ? currentSetting.lightIntensityMultiplier : 1.0f;
@@ -26,28 +39,181 @@ namespace Environment
             Instance = this;
         }
 
+        private void Start()
+        {
+            if (TimeManager.Instance != null)
+            {
+                TimeManager.Instance.OnMinuteChanged -= CheckSchedule;
+                TimeManager.Instance.OnMinuteChanged += CheckSchedule;
+            }
+            
+        }
+
+        private void OnDestroy()
+        {
+            if (TimeManager.Instance != null)
+            {
+                TimeManager.Instance.OnMinuteChanged -= CheckSchedule;
+            }
+        }
         public void InitializeWeather()
         {
             skySettingsMap.Clear();
             
             foreach (SkySettingsSO settings in skySettings)
             {
-                skySettingsMap.TryAdd(settings.weatherType, settings);
+                var weatherState = (settings.timePeriod, settings.weatherType);
+                skySettingsMap.TryAdd(weatherState, settings);
+            }
+            currentTimePeriod=GetTimePeriod(TimeManager.Instance.CurrentTime);
+            RefreshEnvironment(isInstant:true);
+        }
+
+        private TimePeriod GetTimePeriod(DateTime time)
+        {
+            int currentMinutes= time.Hour * 60 + time.Minute;
+            
+            if (dailySchedules.Count == 0)                                                                                                                                               
+                return TimePeriod.Day;
+            
+            TimePeriod resultPeriod = dailySchedules[dailySchedules.Count - 1].timePeriod;
+            int maxpastMinutes = -1;
+
+            foreach (var schedule in dailySchedules)
+            {
+                int periodMinutes =schedule.triggerHour * 60 + schedule.triggerMinute;
+
+                if (periodMinutes <= currentMinutes && periodMinutes > maxpastMinutes)
+                {
+                    maxpastMinutes = periodMinutes;
+                    resultPeriod = schedule.timePeriod;
+                }
             }
             
-            ChangeWeather(WeatherType.Day_Sunny);
+            return resultPeriod;
         }
-
-        public void ChangeWeather(WeatherType weatherType)
+        private void CheckSchedule(DateTime currentTime)
         {
-            if (!skySettingsMap.TryGetValue(weatherType, out SkySettingsSO newSettings))
+            if(isTransitioning)
                 return;
-            currentSetting = newSettings;
+
+            TimePeriod targetTimePeriod = GetTimePeriod(currentTime);
+            if (targetTimePeriod != currentTimePeriod)
+            {
+                bool shouldInstant = false;
+                
+                int oldIndex = dailySchedules.FindIndex(x => x.timePeriod == currentTimePeriod);
+                int newIndex = dailySchedules.FindIndex(x => x.timePeriod == targetTimePeriod);
+
+                if (oldIndex != -1 && newIndex != -1)
+                {
+                    int count = dailySchedules.Count;
+                    int distance = (newIndex - oldIndex + count) % count;
+                    if (distance > 1)
+                        shouldInstant = true;
+                }
+                currentTimePeriod = targetTimePeriod;
+                RefreshEnvironment(isInstant : shouldInstant);
+            }
+        }
+        
+        public void ChangeWeather(WeatherType newWeatherType)
+        {
+            if(currentWeatherType == newWeatherType)
+                return;
             
-            ChangeSkySettings(currentSetting);
+            currentWeatherType = newWeatherType;
+            RefreshEnvironment(isInstant:false);
         }
 
-        private void ChangeSkySettings(SkySettingsSO settings)
+        private void RefreshEnvironment(bool isInstant)
+        {
+            var targetWeatherState = (currentTimePeriod, currentWeatherType);
+            if (!skySettingsMap.TryGetValue(targetWeatherState, out SkySettingsSO targetSetting))
+            {
+                Debug.LogWarning($"({currentTimePeriod}, {currentWeatherType}) 没有配置 SkySettingsSO，请检查 skySettings 列表");
+                return;
+            }
+
+            if (targetSetting == currentSetting)
+                return;
+            
+            if (isInstant)
+            {
+                currentSetting = targetSetting;
+                ChangeSkySettingsInstant(currentSetting);
+            }
+            else
+            {
+                _ = ChangeSkySettingsAsync(targetSetting);
+            }
+        }
+
+        private async Task ChangeSkySettingsAsync(SkySettingsSO targetSetting)
+        {
+            isTransitioning = true;
+
+            Color startSunLightColor = sunLight != null ? sunLight.color : Color.white;
+            Color startAmbientColor = RenderSettings.ambientLight;
+            
+            //静态转程序化
+            Color startSkyTint = Color.gray;
+            Color startGroundColor = Color.gray;
+
+            if (RenderSettings.skybox != null && RenderSettings.skybox.HasProperty("_SkyTint"))
+            {
+                startSkyTint = RenderSettings.skybox.GetColor("_SkyTint");
+                startGroundColor = RenderSettings.skybox.GetColor("_GroundColor");
+            }
+            
+            float elapsedTime = 0f;
+            bool isSkyboxChanged = false;
+
+            while (elapsedTime < transitionDuration)
+            {
+                elapsedTime += Time.deltaTime;
+                float t = elapsedTime / transitionDuration;
+                
+                if(sunLight!=null)
+                    sunLight.color = Color.Lerp(startSunLightColor, targetSetting.sunLightColor, t);
+                RenderSettings.ambientLight = Color.Lerp(startAmbientColor, targetSetting.ambientLightColor, t);
+
+                if (targetSetting.skyMode == SkyMode.Procedural)
+                {
+                    if (!isSkyboxChanged && targetSetting.skyMaterial != null)
+                    {
+                        RenderSettings.skybox=targetSetting.skyMaterial;
+                        isSkyboxChanged = true;
+                    }
+
+                    if (RenderSettings.skybox != null)
+                    {
+                        RenderSettings.skybox.SetColor("_SkyTint",
+                            Color.Lerp(startSkyTint, targetSetting.proceduralSkyTint, t));
+                        RenderSettings.skybox.SetColor("_GroundColor",Color.Lerp(startGroundColor, targetSetting.proceduralGroundColor, t));
+                    }
+                }
+                else
+                {
+                    if (t >= 0.5f && !isSkyboxChanged && targetSetting.skyMaterial != null)
+                    {
+                        RenderSettings.skybox =  targetSetting.skyMaterial;
+                        DynamicGI.UpdateEnvironment();
+                        isSkyboxChanged = true;
+                    }
+                }
+
+                await Task.Yield();
+            }
+
+            RenderSettings.fog = targetSetting.enableFog;
+            RenderSettings.fogColor = targetSetting.fogColor;
+            RenderSettings.fogDensity = targetSetting.fogDensity;
+            
+            currentSetting = targetSetting;
+            isTransitioning = false;
+        }
+        private void ChangeSkySettingsInstant(SkySettingsSO settings)
         {
             if (settings.skyMaterial == null)
                 return;
